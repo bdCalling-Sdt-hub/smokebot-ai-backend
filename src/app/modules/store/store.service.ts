71f8e3f5d981ad99fc9fc6120e35424f2ff160ca
manik <maniksarker652@gmail.com>
2025-05-22 09:27:23 +0600

    craete service for update store profile

--- src/app/modules/store/store.service.ts
+++ src/app/modules/store/store.service.ts
@@ -1,15 +1,18 @@
-import httpStatus from "http-status";
-import AppError from "../../error/appError";
-import { IStore } from "./store.interface";
-import storeModel from "./store.model";
+import httpStatus from 'http-status';
+import AppError from '../../error/appError';
+import { IStore } from './store.interface';
+import storeModel from './store.model';
 
-const updateUserProfile = async (id: string, payload: Partial<IStore>) => {
-    if (payload.email || payload.username) {
-        throw new AppError(httpStatus.BAD_REQUEST, "You cannot change the email or username");
+const updateStoreProfile = async (id: string, payload: Partial<IStore>) => {
+    if (payload.email) {
+        throw new AppError(
+            httpStatus.BAD_REQUEST,
+            'You cannot change the email'
+        );
     }
     const user = await storeModel.findById(id);
     if (!user) {
-        throw new AppError(httpStatus.NOT_FOUND, "Profile not found");
+        throw new AppError(httpStatus.NOT_FOUND, 'Profile not found');
     }
     return await storeModel.findByIdAndUpdate(id, payload, {
         new: true,
@@ -17,5 +20,5 @@ const updateUserProfile = async (id: string, payload: Partial<IStore>) => {
     });
 };
 
-const StoreServices = { updateUserProfile };
+const StoreServices = { updateStoreProfile };
 export default StoreServices;
