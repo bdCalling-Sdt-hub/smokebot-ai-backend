70a36eafd9add6d0a386d65cfd51b8056d6dc9a7
manik <maniksarker652@gmail.com>
2025-06-01 11:31:48 +0600

    update the subscirpotion plan

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ async function main() {
         //     seedSuperAdmin();
         // });
 
-        // initializeSocket(myServer);
+        // initializeSocket(myServer);--
         myServer = server.listen(port, '0.0.0.0', () => {
             logger.info(`Server running on http://0.0.0.0:${port}`);
             seedSuperAdmin();
